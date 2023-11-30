f6cd4d983d7a9a4d1877e5b8198b3db276c79b50
yuhqns <144075745+yuhqns@users.noreply.github.com>
2023-11-30 15:49:37 +0800

    Add files via upload

--- EX1/main.c
+++ EX1/main.c
@@ -1,3 +1,13 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
 int main() {
     printf("*****\n*****\n*****\n*****\n*****\n");
     return 0;
