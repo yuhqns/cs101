b72e96feb8a4cd4b70e5ce9abf0c3a114f8e6c5e
yuhqns <144075745+yuhqns@users.noreply.github.com>
2023-12-14 14:25:15 +0800

    main1.c

--- EX2/main1.c
+++ EX2/main1.c
@@ -1,1 +1,20 @@
+/******************************************************************************
 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int i = 8;
+    if (i &(i-1)) {
+        printf("false");
+    }
+    else {
+        printf("true");
+    }
+    return 0;
+}
