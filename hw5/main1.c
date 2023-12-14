41a1d865d25bc5b4ffaeb37969abac1344ee1e7e
yuhqns <144075745+yuhqns@users.noreply.github.com>
2023-12-14 15:04:01 +0800

    main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,1 +1,21 @@
+/******************************************************************************
 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int n = 7;
+    for (int i=1 ; i<=n ; i++){
+        printf("%*c", n-i+1, ' ');
+        for (int j=1 ; j<=i ; j++){
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
