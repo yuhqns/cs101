96e4a062c9cee3e6ae82f91a6bd9f3b12b0e679e
yuhqns <144075745+yuhqns@users.noreply.github.com>
2023-12-14 14:49:11 +0800

    main7.c

---
+++ EX2/main7.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int i = 2;
+    printf("%.5f, i * 3.1415926535");
+    return 0;
+}
